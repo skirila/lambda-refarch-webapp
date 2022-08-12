7123aea411a502b4f0cb2d7380a1a99ff547a53b
Jiaxi Song <jiaxison@amazon.com>
2022-08-12 15:38:33 +0800

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "lmauo3snau56fs8j3285pdm4b",     // CognitoClientID
+  "api_base_url": "https://conwqnn6ef.execute-api.us-west-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-amplifyteststack.auth.us-west-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1yekonycuuvkr.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
